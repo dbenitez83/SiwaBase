353640815047cec921e4fc3a40332cfc7f979f52
dbenitez83@live.com <dbenitez@uoc.edu>
2020-04-19 11:14:01 +0200

    Funcionando

--- docker/master/config/bootstrap.sh
+++ docker/master/config/bootstrap.sh
@@ -11,18 +11,36 @@ $HADOOP_HOME/sbin/start-yarn.sh
 #$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/root/input_names
 #$HADOOP_HOME/bin/hdfs dfs -copyFromLocal /data/user_ids_answers input_answers
 #$HADOOP_HOME/bin/hdfs dfs -copyFromLocal /data/user_ids_names input_names
-#$HADOOP_HOME/bin/hdfs dfs -mkdir /spark-logs
+$HADOOP_HOME/bin/hdfs dfs -mkdir /spark-logs
 
 # start spark history server
 $SPARK_HOME/sbin/start-history-server.sh
 
 # run the spark job
 #spark-submit --deploy-mode cluster --master yarn \
- #              --class StackAnswer \
- #              $SPARK_HOME/jars/stackanswer_2.12-1.0.jar
+#               --class StackAnswer \
+#              $SPARK_HOME/jars/stackanswer_2.12-1.0.jar
 
 # copy results from hdfs to local
 #$HADOOP_HOME/bin/hdfs dfs -copyToLocal /user/root/users_most_actives /data
 #$HADOOP_HOME/bin/hdfs dfs -copyToLocal /user/root/locations_most_actives /data
+
+$HADOOP_HOME/bin/hadoop fs -mkdir       /tmp
+$HADOOP_HOME/bin/hadoop fs -mkdir       /user
+$HADOOP_HOME/bin/hadoop fs -mkdir       /user/hive
+$HADOOP_HOME/bin/hadoop fs -mkdir       /user/hive/warehouse
+$HADOOP_HOME/bin/hadoop fs -chmod g+w   /tmp
+$HADOOP_HOME/bin/hadoop fs -chmod g+w   /user/hive/warehouse
+
+cd /
+./root/anaconda2/bin/jupyter-notebook --ip=0.0.0.0 --port=3333 --no-browser --allow-root &
+cd /data
+echo -e "Runnning..."
+
+#hive --service metastore &
+cd /usr/local/hive/scripts/metastore/upgrade/mysql/
+schematool -initSchema -dbType mysql
+hive --service hiveserver2 &
+
 
 bash
